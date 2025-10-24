1d9323c21cdfb434e9907d10592bc3744773bf28
Tom Oliver <tom@inmail.me>
2025-10-24 10:53:55 +0200

    loaded trial data to sqlite database

--- data/load_data_to_db.py
+++ data/load_data_to_db.py
@@ -5,24 +5,27 @@ from pathlib import Path
 import pandas as pd
 import requests
 from sqlalchemy import create_engine
-
+import os
 
 # Function to download a selection of CSV files from the web
-def download_csv(url:str) -> None:
+def download_csv(url:str) -> str:
     """
     Download a CSV file from a given URL and save it locally.
     
     Args:
         url (str): The URL of the CSV file to download.
 
+    Returns:
+        str: The filename of the downloaded CSV file.
     """
     # Download the file from the URL
     response = requests.get(url, allow_redirects=True)
-    # Extract the filename from the URL
-    filename = url.split("/")[-1]
+    # Extract the filename from the URL, cleaning any unwanted characters
+    filename = url.split("/")[-1].replace("%20(1)", "")
     # Save the file to the current directory
-    with Path(filename).open('wb') as file:
+    with Path("data",filename).open('wb') as file:
         file.write(response.content)
+    return filename
 
 # Function to load a CSV file as a pandas DataFrame
 def load_csv_to_dataframe(file_path: str) -> pd.DataFrame:
@@ -36,7 +39,7 @@ def load_csv_to_dataframe(file_path: str) -> pd.DataFrame:
         pd.DataFrame: The loaded DataFrame.
 
     """
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(Path("data", file_path))
     df.dropna(inplace=True)
     return df
 
@@ -56,3 +59,15 @@ def load_dataframe_to_db(df: pd.DataFrame, table_name: str,
     engine = create_engine(db_connection_string)
     # Load the DataFrame into the database
     df.to_sql(table_name, con=engine, if_exists='replace', index=False)
+
+if __name__ == "__main__":
+    csv_urls = [
+        "https://raw.githubusercontent.com/MainakRepositor/Datasets/refs/heads/master/water_potability.csv",
+        "https://raw.githubusercontent.com/MainakRepositor/Datasets/refs/heads/master/train.csv",
+        "https://raw.githubusercontent.com/MainakRepositor/Datasets/refs/heads/master/population_by_country_2020%20(1).csv"
+    ]
+    for csv_url in csv_urls:
+        file_name = download_csv(csv_url)
+        df = load_csv_to_dataframe(file_name)
+        load_dataframe_to_db(df, table_name=file_name.split(".")[0],
+                             db_connection_string="sqlite:///data/data.db")
